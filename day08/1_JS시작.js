1ee26df664d21ecb2cefca9c485fe937cd2ee5bf
KimJohann <rhwy1321@gmail.com>
2024-11-21 09:35:51 +0900

    feat : 241120 js변수

---
+++ day08/1_JS시작.js
@@ -0,0 +1,22 @@
+console.log(`[2]안녕 JS`);
+
+/* 1. JS 출력 관련 기능/이벤트/함수 */
+console.log(`[3]console 함수 출력`) //개발자도구의 console 탭에 출력하는 함수
+
+/* 2. alert */
+//alert(`[4]alert 함수 출력`);/* 브라우저 알림창에 내용 출력하는 함수 */
+
+/* 3. document.write() */
+//document.write(`[5] document.write 함수 출력<br />`); //현재 html에 내용을 출력하는 함수
+
+/* 4. document.body.innerHtml */
+//document.body.innerHTML += `[6] document.body.innerHTML 속성출력`;
+
+/* confirm */
+//window.confirm(`[7] 확인 메시지창`);
+
+/* prompt */
+//window.prompt(`[8] 내용 입력 메시지창`) //브라우저 메시지 입력 알림창
+
+/* document.querySelector('마크업명') */
+//document.querySelector('body').innerHTML = `ㄴㅁㅇㄻㄴㅇㄹ`;
